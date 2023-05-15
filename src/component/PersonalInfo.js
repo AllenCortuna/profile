2d01580b85d2064681a3998deefaf4ca5c0e0e7a
zan <olsencortuna@gmail.com>
2023-05-15 19:36:44 +0800

    meta tags changes

--- src/component/PersonalInfo.js
+++ src/component/PersonalInfo.js
@@ -46,13 +46,13 @@ const Baner = () => {
           className="flex flex-col p-4 w-auto text-center">
           <span className="w-auto text-center">
           {letters.map((letter) => (
-            <p className="mt-5 text-white text-xl font-[800] text-[3.6rem] md:text-[4rem] leading-[2.6rem] md:leading-[4rem] hover:text-lime-400 md:text-[4.6rem] hover:text-[4.4rem] md:hover:text-[5.2rem] letter transition-all ease-linear duration-400 hover:animate-bounce inline uppercase mx-[0.1rem]">
+            <p className="mt-5 text-white font-[800] text-[3.5rem] md:text-[4rem] leading-[2.8rem] md:leading-[4rem] hover:text-lime-400 md:text-[4.6rem] hover:text-[4.4rem] md:hover:text-[5.2rem] letter transition-all ease-linear duration-400 hover:animate-bounce inline uppercase mx-[0.1rem]">
               {letter}
             </p>
           ))}
           </span>
 
-          <p className="text-gray-200 font-[400] w-[18rem] space md:text-md text-sm mt-3 md:w-[22rem] mx-auto pt-5 md:pt-10"
+          <p className="text-gray-200 font-[400] w-[16rem] space md:text-md text-sm mt-3 md:w-[22rem] mx-auto pt-5 md:pt-10"
             onMouseEnter={() => setStat("info", "Basic info")}>
             Front end developer that is specialized in React Js, Prototype
             different project using HTML, CSS and JavaScript. Mastery of
